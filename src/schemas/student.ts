1ac119ef0a2a5891668a84f5b90d10126aea7e5f
Eric Santos <ericsantosbr2@gmail.com>
2024-06-10 12:29:00 -0300

    first dummy student schema implementation

---
+++ src/schemas/student.ts
@@ -0,0 +1,18 @@
+import { Mongoose } from "mongoose";
+const { Schema } = Mongoose;
+
+/**
+ * Required studentData follows:
+ * @param studentData.ID
+ * @param studentData.name
+ * @param studentData.birthDate
+ * @param studentData.cpf
+ * @param studentData.classes
+ * @param studentData.grades
+ * @param studentData.notes
+ */
+
+export default function createSchema(studentData) {
+    
+}
+
