86d823c258e3c2a9eaad3e3acd6967da5ebc8c09
Eric Santos <ericsantosbr2@gmail.com>
2024-06-11 09:55:58 -0300

    implemented student apis

--- src/schoolAPIs/students.ts
+++ src/schoolAPIs/students.ts
@@ -1,23 +1,20 @@
 import { Router, Request, Response, NextFunction } from "express";
+import { getStudent, createStudent, deleteStudent } from "../helpers/studentHelpers";
 
 export const studentsRouter: Router = Router();
 
-studentsRouter.get('/:id', (req: Request, res: Response, next: NextFunction) => {
-    let responseJson = {
-        id: req.params.id,
-        name: 'Eric'
-    }
+studentsRouter.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
+    const studentId = req.params.id;
 
-    res.json(responseJson);
+    let studentSearchResult = await getStudent(studentId);
+
+    res.json(studentSearchResult);
 
     next();
 });
 
 studentsRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
-    let responseJson = {
-        id: req.params.id,
-        name: 'Eric'
-    }
+    let responseJson = createStudent(req.body);
 
     res.json(responseJson);
 
